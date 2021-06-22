05af39ad287c6e2a60798a987cde92618b6ee32a
Egor Isichenko <eisichenko@mail.ru>
2021-06-22 17:04:30 +0300

    fixed docker script

--- .scripts/docker_push.sh
+++ .scripts/docker_push.sh
@@ -4,7 +4,7 @@ branch=${GITHUB_REF##*/}
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 
-if [ "${GITHUB_REF##*/}" = "production" ]; then
+if [ "${GITHUB_REF##*/}" == "production" ]; then
     TAG="latest"
 else
     TAG="$branch"
